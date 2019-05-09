b5314161635ee62c23785b87a101999f62b71837
eloyrj <48245099+eloyrj@users.noreply.github.com>
2019-05-09 17:35:13 +0200

    Update Petacereza.java

--- PlantasVsZombiesV2/src/Personajes/Petacereza.java
+++ PlantasVsZombiesV2/src/Personajes/Petacereza.java
@@ -34,7 +34,7 @@ public class Petacereza extends Planta {
         if (getExcepcion().dentroTablero(x, y, j.getPartida().getTablero().lonX(), j.getPartida().getTablero().lonY()) && getExcepcion().casillaOcupada( j.getPartida().getTablero().getTableroPos(x, y)) && getExcepcion().costePosible(this.getCoste(), j.getPartida().getSoles())){
             j.getPartida().getTablero().addT(x, y, this);        
             j.getPartida().setSoles(j.getPartida().getSoles()-getCoste());
-            j.getPartida().setTurno(j.getPartida().getTurno()+1);    
+            j.getPartida().setTurno(j.getPartida().getTurno()+1);
         }
     }
 
@@ -54,6 +54,11 @@ public class Petacereza extends Planta {
             if ( this.getPosX()!= 1 ){
                 if (j.getPartida().getTablero().getTableroPos(super.getPosX()-1, super.getPosY() ) instanceof Zombie  &&  !j.getPartida().getTablero().getTableroPos(super.getPosX()-1, super.getPosY() ).muerto()){
                     Ataque(j.getPartida().getTablero().getTableroPos(super.getPosX()-1, super.getPosY()));
+                }
+            }
+            if ( this.getPosX()!= j.getPartida().getTablero().lonX()){
+                if (j.getPartida().getTablero().getTableroPos(super.getPosX()-1, super.getPosY() ) instanceof Zombie  &&  !j.getPartida().getTablero().getTableroPos(super.getPosX()-1, super.getPosY() ).muerto()){
+                    Ataque(j.getPartida().getTablero().getTableroPos(super.getPosX()+1, super.getPosY()));
                 }
             }
             this.setVida(0);
