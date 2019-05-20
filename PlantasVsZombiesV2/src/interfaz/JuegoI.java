d7bb2b1228d14746d37467b723ff3757a5619fac
eloyrj <48245099+eloyrj@users.noreply.github.com>
2019-05-20 08:51:22 +0200

    he quitado los mains

--- PlantasVsZombiesV2/src/interfaz/JuegoI.java
+++ PlantasVsZombiesV2/src/interfaz/JuegoI.java
@@ -884,45 +884,6 @@ public class JuegoI extends javax.swing.JFrame {
 
     }
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JuegoI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JuegoI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JuegoI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JuegoI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    new JuegoI(new Juego(new Comandos(),new ExcepcionJuego())).setVisible(true);
-                } catch (IOException | ClassNotFoundException ex) {
-                    Logger.getLogger(JuegoI.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
-    }
     
    
     
